5ae9da9a92376a45c5c3a543b153fe9a9e0bec3e
angular-cli <angular-cli@angular.io>
2017-07-26 12:26:43 -0400

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { TypescriptIntroPage } from './app.po';
+
+describe('typescript-intro App', function() {
+  let page: TypescriptIntroPage;
+
+  beforeEach(() => {
+    page = new TypescriptIntroPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
